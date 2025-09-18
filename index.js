b329945067214630e17875a01f6404677d5092ec
karim <karim.sibai77@Hotmail.com>
2025-09-18 11:51:02 +0300

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('Hello Devops!');
+console.log('I am using CI/CD using GitHub Actions');
---
+++ test.js
@@ -0,0 +1,7 @@
+console.log('Starting tests...');
+setTimeout(() =>
+{
+    console.log("Waiting 3 sexonds ....")
+}, 3000);
+
+console.log('test complete');
